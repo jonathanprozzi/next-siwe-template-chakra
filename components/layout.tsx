886eb819b74520961d1e44288f96f884a5182c81
jonathanprozzi <jonathanprozzi@gmail.com>
2023-10-18 16:39:27 -0400

    cleaning up and adding in the env.example

---
+++ components/layout.tsx
@@ -0,0 +1,27 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+// this is the main page layout for the app
+
+export default function Layou({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center pb-4 px-0 ${inter.className} bg-slate-50 text-slate-800`}
+    >
+      {/* Header */}
+      <header className="shadow-md w-full p-4">
+        <div className="mx-auto">
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="text-4xl font-semibold">Next.js SIWE Multiplayer</h1>
+            <ConnectButton />
+          </div>
+        </div>
+      </header>
+      {/* Main Content */}
+      <main className="mx-auto p-4 mt-6">{children}</main>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,90 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  getDefaultWallets,
+  connectorsForWallets,
+} from '@rainbow-me/rainbowkit';
+import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth';
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { polygon, optimism, arbitrum, base, zora, goerli } from 'viem/chains';
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import '@/styles/globals.css';
+import { Component } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID ?? '' }),
+    publicProvider(),
+  ]
+);
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID ?? '';
+
+const { wallets } = getDefaultWallets({
+  appName: 'Next.js SIWE Template',
+  projectId,
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'Next.js SIWE Template',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+  return (
+    <ChakraProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <RainbowKitSiweNextAuthProvider>
+            <RainbowKitProvider chains={chains} appInfo={demoAppInfo}>
+              <Component {...pageProps} />
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </SessionProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 }
